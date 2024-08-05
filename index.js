f3b478ef6f153ad56d2bc3e519758eb793a6f659
sachin <sachinb0841@gmail.com>
2024-08-05 16:38:58 +0530

    dist issue slove

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ require("dotenv").config({ path: "./.env" })
 
 const app = express()
 app.use(cookieParser())
+app.use(express.static(path.join(__dirname, "dist")))
 app.use(express.static("uploads"))
 app.use(cors({
     origin: "http://localhost:5173",
