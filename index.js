1b0c54dbf756bc7965e2b3eb16aaa0ded3aded99
sachin <sachinb0841@gmail.com>
2024-08-06 11:08:46 +0530

    static fix

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require("dotenv").config({ path: "./.env" })
 const app = express()
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, "dist")))
-app.use(express.static("uploads"))
+app.use(express.static(path.join(__dirname, "uploads")))
 app.use(cors({
     origin: "http://localhost:5173",
     credentials: true
